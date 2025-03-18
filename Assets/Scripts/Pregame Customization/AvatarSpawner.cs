c4077c5c0447b6534753e67239d2e5a2415e201d
Sivaraghavi U.R. <sivaraghavi6103@gmail.com>
2025-03-18 16:34:37 +0530

    Avatar Name Customization

--- Assets/Scripts/Pregame Customization/AvatarSpawner.cs
+++ Assets/Scripts/Pregame Customization/AvatarSpawner.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 public class AvatarSpawner : MonoBehaviour
 {
     public GameObject dummyAvatarPrefab;
+    public Transform xrOriginParent; // Drag XR Origin's Camera Offset here
 
     void Start()
     {
-        Instantiate(dummyAvatarPrefab);
+        GameObject avatar = Instantiate(dummyAvatarPrefab, xrOriginParent);
+        avatar.transform.localPosition = Vector3.zero;
+        avatar.transform.localRotation = Quaternion.identity;
     }
 }
