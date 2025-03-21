3a0f4e74b523ac473f80b8cb5bcfaf96cc65500b
Sivaraghavi U.R. <sivaraghavi6103@gmail.com>
2025-03-21 16:00:16 +0530

    DataStore for Customization And Networking

--- Assets/Scripts/Multiplayer/NetworkedAvatar.cs
+++ Assets/Scripts/Multiplayer/NetworkedAvatar.cs
@@ -5,10 +5,9 @@ using UnityEngine;
 
 public class NetworkedAvatar : RealtimeComponent<NetworkedAvatar.Model>
 {
-    // === Model Definition ===
+    #region Model Definition
     public class Model : RealtimeModel
     {
-        // Synced properties
         private string _playerName;
         private int _colorIndex;
 
@@ -19,7 +18,8 @@ public class NetworkedAvatar : RealtimeComponent<NetworkedAvatar.Model>
             {
                 if (_playerName == value) return;
                 _playerName = value;
-                NotifyPropertyChanged();
+
+                // Removed marking dirty since no helper method is available.
             }
         }
 
@@ -30,54 +30,44 @@ public class NetworkedAvatar : RealtimeComponent<NetworkedAvatar.Model>
             {
                 if (_colorIndex == value) return;
                 _colorIndex = value;
-                NotifyPropertyChanged();
+                // Removed marking dirty since no helper method is available.
             }
         }
 
-        // Serialization
         protected override void Write(WriteStream stream, StreamContext context)
         {
-            base.Write(stream, context);
-            stream.WriteString(_playerName);
-            stream.WriteInt(_colorIndex);
+            stream.WriteStructString(_playerName);
+            stream.WriteStructInt(_colorIndex);
         }
 
         protected override void Read(ReadStream stream, StreamContext context)
         {
-            base.Read(stream, context);
-            _playerName = stream.ReadString();
-            _colorIndex = stream.ReadInt();
-            NotifyModelDidChange();
+            _playerName = stream.ReadStructString();
+            _colorIndex = stream.ReadStructInt();
+        }
+
+        protected override int WriteLength(StreamContext context)
+        {
+            return System.Text.Encoding.UTF8.GetByteCount(_playerName) + sizeof(int);
         }
     }
+    #endregion
 
-    // === References ===
+    #region References
     [SerializeField] TextMeshProUGUI nameTag;
     [SerializeField] MeshRenderer[] bodyMeshes;
     [SerializeField] Material[] colorMaterials;
+    #endregion
 
-    // === Model Handling ===
+    #region Model Handling
     protected override void OnRealtimeModelReplaced(Model previousModel, Model currentModel)
     {
-        if (previousModel != null)
-            previousModel.modelDidChange -= OnModelDidChange;
-
-        if (currentModel != null)
-        {
-            if (!currentModel.isFreshModel)
-                UpdateVisuals(currentModel);
-
-            currentModel.modelDidChange += OnModelDidChange;
-        }
+        UpdateVisuals();
     }
+    #endregion
 
-    // === Updates ===
-    private void OnModelDidChange(Model model)
-    {
-        UpdateVisuals(model);
-    }
-
-    private void UpdateVisuals(Model model)
+    #region Visual Updates
+    private void UpdateVisuals()
     {
         nameTag.text = model.playerName;
 
@@ -91,8 +81,9 @@ public class NetworkedAvatar : RealtimeComponent<NetworkedAvatar.Model>
             }
         }
     }
+    #endregion
 
-    // === Initialization ===
+    #region Initialization
     void Start()
     {
         if (realtimeView.isOwnedLocallySelf)
@@ -101,4 +92,110 @@ public class NetworkedAvatar : RealtimeComponent<NetworkedAvatar.Model>
             model.colorIndex = PlayerPrefs.GetInt("ColorIndex", 0);
         }
     }
-}*/
+    #endregion
+}
+*/
+
+using Normal.Realtime;
+using Normal.Realtime.Serialization;
+using TMPro;
+using UnityEngine;
+
+public class NetworkedAvatar : RealtimeComponent<NetworkedAvatar.Model>
+{
+    #region Model Definition
+    public class Model : RealtimeModel
+    {
+        private string _playerName;
+        private int _colorIndex;
+
+        public string playerName
+        {
+            get => _playerName;
+            set
+            {
+                if (_playerName == value) return;
+                _playerName = value;
+                // If your version requires marking the model as dirty,
+                // you may need to add that call here.
+            }
+        }
+
+        public int colorIndex
+        {
+            get => _colorIndex;
+            set
+            {
+                if (_colorIndex == value) return;
+                _colorIndex = value;
+                // Mark model as dirty if required.
+            }
+        }
+
+        protected override void Write(WriteStream stream, StreamContext context)
+        {
+            stream.WriteStructString(_playerName);
+            stream.WriteStructInt(_colorIndex);
+        }
+
+        protected override void Read(ReadStream stream, StreamContext context)
+        {
+            _playerName = stream.ReadStructString();
+            _colorIndex = stream.ReadStructInt();
+        }
+
+        protected override int WriteLength(StreamContext context)
+        {
+            return System.Text.Encoding.UTF8.GetByteCount(_playerName) + sizeof(int);
+        }
+    }
+    #endregion
+
+    #region References
+    [SerializeField] TextMeshProUGUI nameTag;
+    [SerializeField] MeshRenderer[] bodyMeshes;
+    [SerializeField] SkinnedMeshRenderer[] bodySkinnedMeshes;
+    [SerializeField] Material[] colorMaterials;
+    #endregion
+
+    #region Model Handling
+    protected override void OnRealtimeModelReplaced(Model previousModel, Model currentModel)
+    {
+        UpdateVisuals();
+    }
+    #endregion
+
+    #region Visual Updates
+    private void UpdateVisuals()
+    {
+        nameTag.text = model.playerName;
+
+        if (model.colorIndex >= 0 && model.colorIndex < colorMaterials.Length)
+        {
+            foreach (var mesh in bodyMeshes)
+            {
+                var materials = mesh.materials;
+                materials[0] = colorMaterials[model.colorIndex];
+                mesh.materials = materials;
+            }
+            foreach (var skinnedMesh in bodySkinnedMeshes)
+            {
+                Material[] materials = skinnedMesh.materials;
+                materials[0] = colorMaterials[model.colorIndex];
+                skinnedMesh.materials = materials;
+            }
+        }
+    }
+    #endregion
+
+    #region Initialization
+    void Start()
+    {
+        if (realtimeView.isOwnedLocallySelf)
+        {
+            model.playerName = CustomizationData.PlayerName;
+            model.colorIndex = PlayerPrefs.GetInt("ColorIndex", 0);
+        }
+    }
+    #endregion
+}
